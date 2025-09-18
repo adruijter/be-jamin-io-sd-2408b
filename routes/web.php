a5efbace20ca904deef2cbf7aec69c8146e423d5
adruijter <adruijter@gmail.com>
2025-09-18 08:04:24 +0200

    Einde les

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,14 @@ use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AllergeenController;
+
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::get('/Allergeen', [AllergeenController::class, 'index']);
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
